3e2d631be5cea1f77c114c17b8722afe7f394668
Akshay Mulani <97297941+AKSHAYMULANI@users.noreply.github.com>
2025-11-30 10:29:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Other Channel's
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0'))
-request_channel = environ.get('REQUEST_CHANNEL', '0') 
+request_channel = environ.get('REQUEST_CHANNEL', '-1003375963741') 
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0')) 
 
