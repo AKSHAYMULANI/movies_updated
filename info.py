d8d2a32aac509c0e0be5a0d18a458e1ed987b514
Akshay Mulani <97297941+AKSHAYMULANI@users.noreply.github.com>
2025-11-30 10:43:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "marutee")
 
 # Files index database url
 FILES_DATABASE = environ.get('FILES_DATABASE', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'jisshu')
 
 # Other Channel's
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
@@ -54,7 +54,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/akshaymulani')
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+U9oTBETpkpg4YzNl')
 
 # Verification
-IS_VERIFY = is_enabled('IS_VERIFY', True)
+IS_VERIFY = is_enabled(environ.get('IS_VERIFY', 'True'), True)
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/")
